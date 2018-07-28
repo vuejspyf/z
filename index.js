6ee83a1021d859aedcb4039b7ba4156f0d3adfd3
vuejspyf <37735873+vuejspyf@users.noreply.github.com>
2018-07-28 08:11:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var cols=['#ff0000',"#cc00cc","#f4d03f","#ececec","#bcbcbc"];
+var stars=520;
+var txt="林川老师其实有句话一直想对你说，但是没空说;“其实我特别喜欢你---------------------------的座右铭；你必须足够努力才能看起来轻而易举。";
+var index=0;
+for(var i=0;i<=stars;i++){
+var size=Math.random()*3;//生成随机数0-1
+var color=cols[parseInt(Math.random()*6)];//parsein化整
+var span='<span style="width:'+size+'px;height:'+size+'px;top:'+Math.random()*100+'%;left:'+Math.random()*100+'%;background:'+color+';box-shadow:0 0 '+Math.random()*10+'px'+color+';"></span>';
+//console.log(span);
+$("#starbox").prepend(span);
+}
+
+setTimeout(function() {
+  $("#starbox span").each(function(){
+      $(this).css('top',Math.random()*100+'%').css('left',Math.random()*100+'%');
+  })
+},1);
+ typing=function(){
+var love=$(".love p");
+if(index<=txt.length){
+    love.text(txt.slice(0,index++));
+    setTimeout('typing()',300)
+    console.log(index)
+}
+
+}
+typing();
